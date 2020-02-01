fa1f1f95e1a71fa1a669abec49b24dcc01a22a8d
Izripov-Yusuf <izripov1998@gmail.com>
2020-02-01 21:51:04 +0300

    Усложнённое, 6 урок

--- script.js
+++ script.js
@@ -8,29 +8,54 @@ function guess() {
   let randomNumber = Math.floor(Math.random() * Math.floor(100)) + 1;
   console.log('Рандомное число: ', randomNumber);
 
+  let attempts = 10;
+  console.log('Количество попыток: ', attempts);
+
   let numberСheck = function () {
     let userNumber = prompt('Введите число от 1 до 100');
     if (userNumber === null) {
-      alert('Игра закончена');
+      alert('Спасибо за игру');
     } else if (isNumber(userNumber)) {
-    userNumber = +userNumber;
+      userNumber = +userNumber;
       if (userNumber > randomNumber) {
-        alert('Загаданное число меньше!');
+        attempts--;
+        alert('Загаданное число меньше, осталось попыток ' + attempts);
+        if (attempts === 0) {
+          let end = confirm('Попытки закончились, хотите сыграть еще?');
+          if (end === true) {
+            attempts = 10;
+            return guess();
+          }
+          else {
+            alert('Спасибо за игру');
+          }
+        }
         numberСheck();
       } else if (userNumber < randomNumber) {
-        alert('Загаданное число больше!');
+        attempts--;
+        alert('Загаданное число больше, осталось попыток ' + attempts);
+        if (attempts === 0) {
+          let end = confirm('Попытки закончились, хотите сыграть еще?');
+          if (end === true) {
+            attempts = 10;
+            return guess();
+          }
+          else {
+            alert('Спасибо за игру');
+          }
+        }
         numberСheck();
       } else {
-        alert('Вы победили! Поздравляем!');
-        return;
+        alert('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+        return guess();
+
       }
     } else {
       alert('Введи число!');
       numberСheck();
     }
   };
-  return numberСheck;
-  }
+  return numberСheck();
+}
 
-let guessNumber = guess();
-guessNumber();
+guess();
